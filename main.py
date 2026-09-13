538ad6f890efd0316fea17a7aa3676d5a6b61519
alhnynybyan-rgb <alhnynybyan@gmail.com>
2026-09-13 12:39:08 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+name = "bayan"
+major = "electrical engineering - network"
+university = "Bau-Fet"
+print("student profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
